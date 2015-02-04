9a71d1571f352bf465023f024036a658deb563a5
Matt Brandt <mbrandt@mozilla.com>
2015-02-04 16:44:55 -0700

    added refactored tests to use self.server_url + renamed several tests

--- test_wp.py
+++ test_wp.py
@@ -2,64 +2,66 @@ from loads.case import TestCase
 
 class TestWP(TestCase):
 
+	server_url = 'https://blog-dev.allizom.org'
+
 	def test_root_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/')
+		res = self.session.get(self.server_url)
 		self.assertEqual(res.status_code, 200)
 
 	def test_viewing_beard_post_root_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/blog/2014/07/28/chris-beard-named-ceo-of-mozilla/')
+		res = self.session.get(self.server_url + '/blog/2014/07/28/chris-beard-named-ceo-of-mozilla/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_viewing_yahoo_post_root_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/press/2014/11/yahoo-and-mozilla-form-strategic-partnership/')
+		res = self.session.get(self.server_url + '/press/2014/11/yahoo-and-mozilla-form-strategic-partnership/')
 		self.assertEqual(res.status_code, 200)
 
-	def test_negative_search(self):
-		res = self.session.get('https://blog-dev.allizom.org/?s=orcs')
+	def test_negative_search_root_blog(self):
+		res = self.session.get(self.server_url + '/?s=orcs')
 		self.assertEqual(res.status_code, 200)
 
 	def test_positive_beard_search_root_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/?s=beard')
+		res = self.session.get(self.server_url + '/?s=beard')
 		self.assertEqual(res.status_code, 200)
 
-	def test_postitive_yahoo_search_root_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/press/?s=Yahoo')
+	def test_postitive_yahoo_search_press_blog(self):
+		res = self.session.get(self.server_url + '/press/?s=Yahoo')
 		self.assertEqual(res.status_code, 200)
 
 	def test_rss_feeds_root(self):
-		res = self.session.get('https://blog-dev.allizom.org/feed/')
+		res = self.session.get(self.server_url + '/feed/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_ux_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/ux/')
+		res = self.session.get(self.server_url + '/ux/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_planet_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/planet/')
+		res = self.session.get(self.server_url + '/planet/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_webqa_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/webqa/')
+		res = self.session.get(self.server_url + '/webqa/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_webqa_mbrandt_file_image(self):
-		res = self.session.get('https://blog-dev.allizom.org/webqa/files/2015/01/mbrandt.jpg')
+		res = self.session.get(self.server_url + '/webqa/files/2015/01/mbrandt.jpg')
 		self.assertEqual(res.status_code, 200)
 
 	def test_webdev_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/webdev/')
+		res = self.session.get(self.server_url + '/webdev/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_sumo_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/sumo/')
+		res = self.session.get(self.server_url + '/sumo/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_press_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/press/')
+		res = self.session.get(self.server_url + '/press/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_openstandards_blog(self):
-		res = self.session.get('https://blog-dev.allizom.org/openstandard-en/')
+		res = self.session.get(self.server_url + '/openstandard-en/')
 		self.assertEqual(res.status_code, 200)
 
 	def test_positive_search_javascript_hacks_blog(self):
